1d228e509c069ee6f28942e5713d601e06ea9874
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2025-05-24 15:24:44 -0600

    add custom keymaps

--- init.lua
+++ init.lua
@@ -198,6 +198,12 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- quickfix naviation
+vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>', { desc = 'Next in quickfix list' })
+vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>', { desc = 'Prev in quickfix list' })
+-- jk/kj simultaneously to exit insert mode
+vim.keymap.set('i', 'jk', '<ESC>')
+vim.keymap.set('i', 'kj', '<ESC>')
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
