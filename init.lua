1dc4aef444684c938ee3e9f98a9feeae7010e097
KerrickJohnstonbaugh <kjohnstonbaugh97@gmail.com>
2025-05-24 17:38:29 -0600

    configure formatting

--- init.lua
+++ init.lua
@@ -776,21 +776,22 @@ require('lazy').setup(
       opts = {
         notify_on_error = false,
         format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
-          -- have a well standardized coding style. You can add additional
-          -- languages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
-          if disable_filetypes[vim.bo[bufnr].filetype] then
-            return nil
-          else
-            return {
-              timeout_ms = 500,
-              lsp_format = 'fallback',
-            }
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
           end
+          return { timeout_ms = 500, lsp_format = 'fallback' }
         end,
         formatters_by_ft = {
           lua = { 'stylua' },
+          python = {
+            -- To fix auto-fixable lint errors.
+            'ruff_fix',
+            -- To run the Ruff formatter.
+            'ruff_format',
+            -- To organize the imports.
+            'ruff_organize_imports',
+          },
           -- Conform can also run multiple formatters sequentially
           -- python = { "isort", "black" },
           --
@@ -1054,6 +1055,28 @@ require('lazy').setup(
 
 -- Required: Enable the language server
 vim.lsp.enable 'ty'
+
+-- stuff to disable format on save
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
+-- call this on init to disable
+vim.api.nvim_command 'FormatDisable'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
